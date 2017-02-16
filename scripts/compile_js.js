38cb9f3fb67c4b1616dcaa5b11ab8351911af206
James Monger <jameskmonger@hotmail.co.uk>
2017-02-16 14:02:36 +0000

    Don't use webpack

---
+++ scripts/compile_js.js
@@ -0,0 +1,44 @@
+var babel = require('babel-core');
+var fs = require('fs');
+var path = require('path');
+
+var inputPath = path.resolve(__dirname, '../src/PercentageCircle.js');
+var outputPath = path.resolve(__dirname, '../build/index.js');
+
+// make the final JS changes
+var finaliseJs = (js) => {
+  return js.replace("./style/PercentageCircle.scss", "./style.css");
+}
+
+var writeJs = (js, callback) => {
+  // make any final changes we need
+  var finalisedJS = finaliseJs(js);
+
+  fs.writeFile(outputPath, finalisedJS, (err) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    callback(null);
+  });
+};
+
+var transformComplete = (callback) => {
+  return (err, result) => {
+    // if there was an error transforming, bubble it up
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    // if there was no error, write the JS
+    writeJs(result.code, (err) => callback(err));
+  };
+}
+
+module.exports = (callback) => {
+  babel.transformFile(inputPath, {
+    presets: ["react", "latest"]
+  }, transformComplete(callback));
+}
