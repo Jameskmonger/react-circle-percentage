649867f0b0e8f1c72bbbca226e40ae619a2c2c86
James Monger <jameskmonger@hotmail.co.uk>
2017-03-15 12:02:30 +0000

    Add 'show' to section

--- src/js/components/section.js
+++ src/js/components/section.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Section extends React.Component {
   render() {
-    return <section>
+    return <section className={`${ this.props.show ? 'show' : '' }`}>
       { this.props.children }
     </section>;
   }
